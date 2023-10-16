8dc954da19815fd17dce35b5e15c79eb8b748a8f
YueNiu <yueniuuw@gmail.com>
2023-10-16 14:54:32 +1100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,255 @@
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // 必须加上这一行。
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UniTab UI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'UniTab'),
+    );
+  }
+}
+
+// 监听事件
+class HomePage extends StatefulWidget {
+  final String title;  // 定义一个title变量
+
+  const HomePage({Key? key, required this.title}) : super(key: key);
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> with WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+    _init();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  void _init() async {
+    // 添加此行以覆盖默认关闭处理程序
+    await windowManager.setPreventClose(true);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: const CircleAvatar(
+          backgroundImage: AssetImage('path_to_your_image.png'),
+          backgroundColor: Colors.white,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('path_to_your_image.png'),
+              radius: 40.0,
+            ),
+            const SizedBox(height: 10),
+            const Text('Username'),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true, // 使ListView只占用其子部件所需的空间
+                children: [
+                  NoteTile(title: "Today is Monday..."),
+                  NoteTile(title: "1. Eat breakfast\n2. Read a book\n3. Assignment"),
+                  // ... 其他ListView子部件
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  @override
+  void onWindowEvent(String eventName) {
+    print('[WindowManager] onWindowEvent: $eventName');
+  }
+
+  @override
+  void onWindowClose() async {
+    final localContext = context; // 使用一个局部变量保存BuildContext
+
+    bool _isPreventClose = await windowManager.isPreventClose();
+    if (_isPreventClose) {
+      showDialog(
+        context: localContext, // 使用局部变量
+        builder: (_) {
+          return AlertDialog(
+            title: const Text('Are you sure you want to close this window?'),
+            actions: [
+              TextButton(
+                child: const Text('No'),
+                onPressed: () {
+                  Navigator.of(localContext).pop(); // 使用局部变量
+                },
+              ),
+              TextButton(
+                child: const Text('Yes'),
+                onPressed: () async {
+                  Navigator.of(localContext).pop(); // 使用局部变量
+                  await windowManager.destroy();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+
+  @override
+  void onWindowFocus() {
+    // Make sure to call once.
+    setState(() {});
+    // do something
+  }
+
+  @override
+  void onWindowBlur() {
+    // do something
+  }
+
+  @override
+  void onWindowMaximize() {
+    // do something
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    // do something
+  }
+
+  @override
+  void onWindowMinimize() {
+    // do something
+  }
+
+  @override
+  void onWindowRestore() {
+    // do something
+  }
+
+  @override
+  void onWindowResize() {
+    // do something
+  }
+
+  @override
+  void onWindowMove() {
+    // do something
+  }
+
+  @override
+  void onWindowEnterFullScreen() {
+    // do something
+  }
+
+  @override
+  void onWindowLeaveFullScreen() {
+    // do something
+  }
+}
+
+//note 列表
+class NoteTile extends StatelessWidget {
+  final String title;
+
+  NoteTile({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      elevation: 5,
+      child: ListTile(
+        title: Text(title),
+        trailing: Icon(Icons.star_border),
+        onTap: () {},
+      ),
+    );
+  }
+}
+
+//toolbar
+class CustomTitleBar extends StatelessWidget {
+  const CustomTitleBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50, // 设定你的标题栏高度
+      color: Colors.blue, // 设定标题栏颜色
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              // 这里可以实现打开设置页面或其他操作
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () async {
+              await windowManager.close(); // 使用`window_manager`来关闭应用窗口
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
